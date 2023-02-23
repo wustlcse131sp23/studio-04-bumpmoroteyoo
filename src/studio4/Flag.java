ad68b82d50f2b0e6d1d0db89775d3deaab00b7c3
William Yoo <87338673+vvilliamyoo@users.noreply.github.com>
2023-02-23 15:17:33 -0600

    <3

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,13 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(176, 0, 250);
+		StdDraw.circle(.7, .7, 0.2);
+		StdDraw.setPenColor(255, 0, 255);
+		StdDraw.circle(0.5, 0.5, 0.3);
+		StdDraw.setPenColor(255, 0, 176);
+		double[] x = {0.1,0.2, 0.3};
+		double[] y = {0.5,0.7, 0.3};
+		StdDraw.filledPolygon(x, y);
 	}
 }
